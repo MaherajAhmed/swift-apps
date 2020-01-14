2f9d4ad4bd98865525d0f040d194d85ffc5719ce
MaherajAhmed <ahmed.M@etoncollege.org.uk>
2020-01-14 12:22:51 +0000

    start

---
+++ tamagotchiApp/tamagotchiApp/Tamagotchi.swift
@@ -0,0 +1,9 @@
+//
+//  Tamagotchi.swift
+//  tamagotchiApp
+//
+//  Created by Ahmed, Maheraj (HWTA) on 14/01/2020.
+//  Copyright © 2020 Ahmed, Maheraj (HWTA). All rights reserved.
+//
+
+import Foundation
---
+++ tamagotchiApp/tamagotchiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tamagotchiApp
+//
+//  Created by Ahmed, Maheraj (HWTA) on 14/01/2020.
+//  Copyright © 2020 Ahmed, Maheraj (HWTA). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
