196281165bd12ce7063eeec3b335e0ba5dbdbdb0
MaherajAhmed <ahmed.M@etoncollege.org.uk>
2020-01-17 11:22:16 +0000

    timer added

--- tamagotchiApp/tamagotchiApp/ViewController.swift
+++ tamagotchiApp/tamagotchiApp/ViewController.swift
@@ -9,19 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var timer: Timer?
+    var timeRemaining = Int.random(in: 40...60)
+    var tamagotchi: Tamagotchi? {
+        didSet {
+            tamagotchiStats.text = tamagotchi?.tamagotchiStats()
+            
+        }
+    }
 
+
+    @IBOutlet var remainingTimeDisplay: UILabel!
     @IBOutlet var tamagotchiStats: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tamagotchi = Tamagotchi()
+        var timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector:#selector(countdown), userInfo: nil, repeats: true)
+    }
         
-        var tamagotchiStats = Tamagotchi()
-        var tamagotchi: Tamagotchi?{
-            tamagotchi?.tamagotchiStats()
-        }
+        
+    @objc func countdown() {
+        if timeRemaining > 0 {
+            timeRemaining -= 1
+            remainingTimeDisplay.text = "\(timeRemaining)"
+            tamagotchi?.timeTillToilet = timeRemaining
+            
+        } else {
+            timer?.invalidate()
+              }
+    }
+            
+    
+        
         
         // Do any additional setup after loading the view.
     }
 
 
-}
 
